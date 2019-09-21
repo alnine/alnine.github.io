17a114f6cc8e0fa274419e3578f03d9e655fed4b
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-09-21 12:41:47 +0300

    Adds Tickets project

---
+++ projects/tickets/precache-manifest.356624464087eaa7c387530b6828cd1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22bc41d7c77f72ca34ac3e2b9d245d50",
+    "url": "/projects/tickets/index.html"
+  },
+  {
+    "revision": "01261c05d18d6260ba94",
+    "url": "/projects/tickets/static/css/main.97fd076d.chunk.css"
+  },
+  {
+    "revision": "8add67284a3c78aacdd7",
+    "url": "/projects/tickets/static/js/2.8b628b52.chunk.js"
+  },
+  {
+    "revision": "01261c05d18d6260ba94",
+    "url": "/projects/tickets/static/js/main.aedf20a6.chunk.js"
+  },
+  {
+    "revision": "321191162010e598b5dc",
+    "url": "/projects/tickets/static/js/runtime~main.c8a5c82d.js"
+  },
+  {
+    "revision": "56508a63b2f22b94f41a27f64cc229d8",
+    "url": "/projects/tickets/static/media/logo.56508a63.png"
+  }
+]);
