786d1df40841326295f3913a5c37658c0d186001
Aleksandr Kushniruk <kushniruk_aleksandr@ukr.net>
2020-03-13 11:39:36 +0200

    Fix: wrong links to directories

---
+++ projects/tic-tac-toe/precache-manifest.533b2de0bf3d91342045d8130cda09cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9901a55586be129df30fdf9989fdc7b2",
+    "url": "/projects/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "780dd6dbc568b4538029",
+    "url": "/projects/tic-tac-toe/static/js/2.9fe1dd08.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/projects/tic-tac-toe/static/js/2.9fe1dd08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f834663dbe5c0a43c8",
+    "url": "/projects/tic-tac-toe/static/js/main.9dcc0816.chunk.js"
+  },
+  {
+    "revision": "a0910fde2698dd6b72be",
+    "url": "/projects/tic-tac-toe/static/js/runtime-main.bf08208c.js"
+  }
+]);
--- projects/tic-tac-toe/service-worker.js
+++ projects/tic-tac-toe/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c9d816577f854cc9e8b7e922f93261b.js"
+  "/projects/tic-tac-toe/precache-manifest.533b2de0bf3d91342045d8130cda09cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projects/tic-tac-toe/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
